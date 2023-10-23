c357112e1e2b03695fea31322c19dd133a2db643
Cristian Roman <romancristianroman@yahoo.com>
2023-10-23 18:23:18 +0300

    [Lab3] problem 2 -- done

--- Lab3/main.py
+++ Lab3/main.py
@@ -8,4 +8,9 @@ def GetOperationSetsOverLists(a: list, b: list) -> list[set]:
     return [intersection, union, difference_a, difference_b]
 
 
-print(GetOperationSetsOverLists([1, 2, 3], [2, 3, 4]))
+# print(GetOperationSetsOverLists([1, 2, 3], [2, 3, 4]))
+
+def GetNumberOfLettersInWordDictionary(word: str) -> dict[str, int]:
+    return {letter: word.count(letter) for letter in set(word)}
+
+# print(GetNumberOfLettersInWordDictionary("ana are mere"))
